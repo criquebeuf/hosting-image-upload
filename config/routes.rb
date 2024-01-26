21abf2d1a1bd15456086337de2d32ec9ed17b5e4
Justine Auguste <justine_auguste@hotmail.com>
2024-01-26 11:24:28 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'articles#index'
   resources :articles, except: :index
+  # root become the prefix and we need to remove the root create
+  # by default by the resources (i.e. articles)
 end
